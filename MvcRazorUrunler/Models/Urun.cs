7a34bc22c32ebd3ca1821cfbb13db8354be5460b
Emre Karaman <karaman.66@hotmail.com>
2021-04-18 23:42:19 +0300

    Add project files.

---
+++ MvcRazorUrunler/Models/Urun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcRazorUrunler.Models
+{
+
+    public class Urun
+    {
+        public int Id { get; set; }
+
+        [Required,StringLength(30)]
+        public string UrunAd { get; set; }
+
+        [Required]
+        public decimal BirimFiyat { get; set; }
+    }
+}
